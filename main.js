59401effdacb10fea4c90943b5207de14f32d40e
Rimas-S <96993500+Rimas-S@users.noreply.github.com>
2022-01-06 22:16:57 +0100

    update

--- main.js
+++ main.js
@@ -2,23 +2,28 @@ const form = document.getElementById('form');
 const listContainer = document.getElementById('list-container');
 const removeAll = document.getElementById('btn-remove');
 
+// Add event listener to the "Add" button //
 form.addEventListener('submit', addtaskDiv);
 
+//  Add event listener to the "RemoveAll" button //
 removeAll.addEventListener ('click', removeAllTasks);
 
 
-
+// Add Tasks to the task list funktion //
 function addtaskDiv(e){
     e.preventDefault();
 
+   // Create <div> for task elements //
+   let taskDiv = document.createElement('div');
+   taskDiv.className = 'task-div flex';
+
+   // Create paragraph tag for task text //
    let taskInput = document.createElement('p')
    taskInput.addEventListener('click', checkAsDone);
    taskInput.appendChild(document.createTextNode(document.getElementById('title').value));
 
-   let taskDiv = document.createElement('div');
-   taskDiv.className = 'task-div flex';
 
-  
+   // Create icons //
    let iconDelete = document.createElement('i');
    iconDelete.className = 'far fa-trash-alt';
    iconDelete.addEventListener('click', removetaskDiv)
@@ -26,17 +31,21 @@ function addtaskDiv(e){
    let iconCheck = document.createElement('i');
    iconCheck.className = 'fas fa-check';
 
+   // Add task parts one <div> //
    taskDiv.appendChild(iconCheck);
    taskDiv.appendChild(taskInput);
    taskDiv.appendChild(iconDelete);
 
+   // Add task <div> to the list container //
    listContainer.appendChild(taskDiv);
 
+   // Clear and focus on the input line //
    document.getElementById('title').value = '';
    document.getElementById('title').focus();
 
 }
 
+// Remove single task funktion //
 function removetaskDiv(e){
 
     let taskDiv = e.target.parentElement;
@@ -44,6 +53,7 @@ function removetaskDiv(e){
 
 }
 
+// Mark task as done //
 function checkAsDone(e){
 
     let taskDiv = e.target.parentElement;
@@ -57,6 +67,7 @@ function checkAsDone(e){
 
 }
 
+// Remove all tasks //
 function removeAllTasks(e){
 
     let tasks = document.querySelector('#list-container');
